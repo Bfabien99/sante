abe02988436ee10016ba3bec5986901c9cb843a4
Bfabien99 <91257945+Bfabien99@users.noreply.github.com>
2022-01-28 15:55:39 +0000

    test infermedica api

--- index.php
+++ index.php
@@ -1,2 +1,31 @@
 <?php
-    header('location:view/home.php');
+    require 'models/apiInfermedica.php';
+    $api = new apiInfermedica('d423892e','10676238d756762e2e7ce7bd3bd5f302');
+
+    $info = $api->info();
+    $diagnostis = $api->diagnostic();
+    $symptome = $api->symptome(25);
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="">
+        <select name="" id="">
+        <?php foreach($symptome as $symp):?>
+            <option value="<?= $symp ?>">
+                <?= $symp ?>
+            </option>
+        <?php endforeach?>
+        </select>
+    </form>
+    
+
+    
+</body>
+</html>
---
+++ models/apiInfermedica.php
@@ -0,0 +1,109 @@
+<?php
+
+    class apiInfermedica{
+
+        
+        private $apiId;
+        private $apiKey;
+
+        public function __construct(string $apiId,string $apiKey)
+        {   
+            $this->apiId = $apiId;
+            $this->apiKey = $apiKey;
+        }
+
+        public function info(){
+            $curl = curl_init();
+
+            curl_setopt_array($curl, array(
+            CURLOPT_URL => 'https://api.infermedica.com/v3/info',
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => '',
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => 'GET',
+            CURLOPT_HTTPHEADER => array(
+            'App-Id: '.$this->apiId,
+            'App-Key: '.$this->apiKey),));
+            $response = curl_exec($curl);
+
+            curl_close($curl);
+            return $response;
+
+        }
+
+        public function symptome($age){
+            $curl = curl_init();
+
+            curl_setopt_array($curl, array(
+            CURLOPT_URL => 'https://api.infermedica.com/v3/symptoms?age.value='.$age,
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => '',
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => 'GET',
+            CURLOPT_HTTPHEADER => array(
+                'App-Id: d423892e',
+                'App-Key: 10676238d756762e2e7ce7bd3bd5f302'
+            ),
+            ));
+
+            $response = curl_exec($curl);
+            curl_close($curl);
+            $sickName = [];
+            foreach (json_decode($response) as $key => $sick) {
+                $sickName[] = $sick->name;
+            }
+            return $sickName;
+        }
+
+        public function diagnostic(){
+            $curl = curl_init('https://api.infermedica.com/v3/diagnosis');
+
+curl_setopt_array($curl, array(
+  CURLOPT_RETURNTRANSFER => true,
+  CURLOPT_ENCODING => '',
+  CURLOPT_MAXREDIRS => 10,
+  CURLOPT_TIMEOUT => 0,
+  CURLOPT_FOLLOWLOCATION => true,
+  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+  CURLOPT_CUSTOMREQUEST => 'POST',
+  CURLOPT_POSTFIELDS =>'{
+  "sex": "male",
+  "age": {
+      "value": 30
+  },
+  "evidence": [
+    {
+      "id": "s_1193",
+      "choice_id": "unknown"
+    },
+    {
+      "id": "s_488",
+      "choice_id": "present"
+    },
+    {
+      "id": "s_418",
+      "choice_id": "present"
+    }
+  ]
+}',
+  CURLOPT_HTTPHEADER => array(
+    'App-Id: '.$this->apiId,
+    'App-Key: '.$this->apiKey,
+    'Content-Type: application/json'
+  ),
+));
+
+$response = curl_exec($curl);
+
+curl_close($curl);
+return $response;
+        }
+
+        
+    }
